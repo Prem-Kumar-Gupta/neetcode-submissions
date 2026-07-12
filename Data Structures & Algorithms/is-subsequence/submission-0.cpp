71e2051a6dbeb4fc84583330fa513a115dd90b6b
Prem Kumar Gupta <95561600+Prem-Kumar-Gupta@users.noreply.github.com>
2026-07-12 14:06:38 +0530

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+     int n = s.length();
+     if(n<1){
+        return true;
+     }
+     int q=0;
+     for(int i =0;i<t.length();i++){
+        if(t[i]==s[q]){
+            q++;
+            if(q>n-1){
+                return true;
+            }
+        }
+     }
+     return false;   
+    }
+};
