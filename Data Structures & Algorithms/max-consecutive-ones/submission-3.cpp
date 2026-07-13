ff4cb3eb5af827e078eae86d6c6eb9bf50cf8e43
Prem Kumar Gupta <95561600+Prem-Kumar-Gupta@users.noreply.github.com>
2026-07-13 14:55:42 +0530

    Add: max-consecutive-ones - submission-3

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0 ;
+        int tempCount=0;
+        for(int i = 0 ;i < nums.size();i++){
+            if(nums[i]==1){
+                tempCount++;
+            }else{
+                count=max(count,tempCount);
+                tempCount=0;
+            }
+        }
+        count =max(count,tempCount);
+        return count;
+    }
+};
