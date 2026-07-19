88939206a8906abff08bd589d512fde83c236444
Prem Kumar Gupta <95561600+Prem-Kumar-Gupta@users.noreply.github.com>
2026-07-19 16:29:34 +0530

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-2

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        unordered_map<char,int> m ;
+        for(int i = 0 ; i <s.length();i++){
+            m[s[i]]++;
+        }
+        int odd = 0;
+        int even =INT_MAX;
+        for(auto it = m.begin();it!=m.end();it++){
+            int num = it->second;
+            if(num%2==0){
+                if(num<even){
+                    even=num;
+                }
+            }else{
+                if(num>odd){
+                    odd=num;
+                }
+            }
+        }
+        return odd-even;
+    }
+};
