c87c3ca40dbaa5210ca16821bd76a6804d2157a2
Prem Kumar Gupta <95561600+Prem-Kumar-Gupta@users.noreply.github.com>
2026-09-17 15:31:18 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int size=students.size();
+        int c=0;
+        while(!students.empty() && c < students.size()){
+            size=students.size();
+            int st=students[0];
+            int sa=sandwiches[0];
+            if(st==sa){
+            students.erase(students.begin());
+            sandwiches.erase(sandwiches.begin());
+            c=0;
+            }
+            else if(st!=sa){
+            students.erase(students.begin());
+            students.push_back(st);
+            c++;
+        }
+        }
+        return c;
+    }
+};
