62e7afb02de38a20d61180b2c507bc720b950471
Prem Kumar Gupta <95561600+Prem-Kumar-Gupta@users.noreply.github.com>
2026-09-22 17:30:18 +0530

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int t = 0;
+        int r = 0;
+        int l = 0;
+        for(int i = 0 ; i <nums.size();i++){
+            t+=nums[i];
+        }
+        for(int i = 0 ; i <nums.size();i++){
+            r=t-l-nums[i];
+            if(r==l){
+                return i;
+            }
+            l+=nums[i];
+        }
+        return -1;
+    }
+};
