4a714ab50745220b41db4a49b6fadfc2e7b7a7a2
Prem Kumar Gupta <95561600+Prem-Kumar-Gupta@users.noreply.github.com>
2026-09-24 18:09:39 +0530

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int can = 0;
+        int count =0;
+        for(int i = 0 ; i  <nums.size(); i++){
+            if(count==0){
+                can=nums[i];
+            }
+            if(nums[i]==can){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return can;
+    }
+};
